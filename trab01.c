2456e8ade82c7e64eb87a02415f12d53c8ea53b7
Matheus Marques <martreus@gmail.com>
2016-04-30 20:32:28 -0300

    Declaração de variáveis
    Foram declaradas as variáveis a serem utilizadas pelo programa.

---
+++ trab01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    float bound[5];
+
+    bound[0] = 0;
+    bound[1] = 1903.98;
+    bound[2] = 2826.65;
+    bound[3] = 3751.05;
+    bound[4] = 4664.68;
+
+    printf("%.2f", bound[2]);
+
+    /*O que deve ser calculado:*/
+    float baseIR;
+    float valorIR;
+    float pctIR;
+
+    /*Deducoes:*/
+
+    float previdenciaOficial;
+    int dependentes;
+    float pensaoAlimenticia;
+    float outrasDeducoes;
+
+    return 0;
+
+
+
+
+
+
+}
